23494ae73d6b112b25a7c65df821e6ca15e327fb
bohlmann <#TCPWJon1707>
2018-05-24 11:22:43 +0200

    - add default dirs to bb-data - enable display errors for installer - add empty bb-config.php for installation - modify .gitignore - update minimun php to composer

--- src/install/index.php
+++ src/install/index.php
@@ -12,7 +12,7 @@ function isSSL() {
 date_default_timezone_set('UTC');
 
 error_reporting(E_ALL);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 ini_set('log_errors', '1');
 ini_set('error_log', dirname(__FILE__) . '/php_error.log');
