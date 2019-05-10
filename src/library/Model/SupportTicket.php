8a718e6ea3359204a4ffb908ebb2611677846aab
Jonah Bohlmann <kontakt@jonah-bohlmann.de>
2019-05-10 17:35:00 +0200

    - fix support ticket issues

--- src/library/Model/SupportTicket.php
+++ src/library/Model/SupportTicket.php
@@ -1,16 +1,28 @@
 <?php
 /**
- * BoxBilling
+ * CircleBilling
  *
  * @copyright BoxBilling, Inc (http://www.boxbilling.com)
+ * @copyright CircleBilling (https://circlebilling.com)
  * @license   Apache-2.0
  *
- * Copyright BoxBilling, Inc
  * This source file is subject to the Apache-2.0 License that is bundled
  * with this source code in the file LICENSE
+ *
  */
 
-
+/**
+ * Class Model_SupportTicket
+ *
+ * @property int id
+ * @property int support_helpdesk_id
+ * @property int client_id
+ * @property int priority
+ * @property string subject
+ * @property string status (open, closed, on:hold)
+ * @property int rel_type
+ *
+ */
 class Model_SupportTicket extends RedBean_SimpleModel
 {
     const OPENED = 'open';
